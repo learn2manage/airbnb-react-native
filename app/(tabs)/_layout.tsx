e7f82a015b8faa9081156cbc66d14706793e42fa
Learn2Manage <Learn2Manage@gmail.com>
2024-03-14 17:38:56 +0800

    editing tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,34 @@
 import { Tabs } from 'expo-router';
 import Colors from '@/constants/Colors';
+import { Ionicons } from '@expo/vector-icons';
 
 const Layout = () => {
-    return <Tabs></Tabs>;
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors.primary,
+      }}
+    >
+      <Tabs.Screen
+        name='index'
+        options={{
+          tabBarLabel: 'Explore',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name='search' size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name='wishlists'
+        options={{
+          tabBarLabel: 'Explore',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name='heart-outline' size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 };
 
 export default Layout;
