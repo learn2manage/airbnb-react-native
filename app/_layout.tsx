35c6ffef52cf04921269e3642556c4eb9e46f19b
Learn2Manage <Learn2Manage@gmail.com>
2024-03-15 16:32:28 +0800

    To work with Clerk Auth and show login screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,8 +5,27 @@ import { useRouter } from 'expo-router';
 import { SplashScreen, Stack } from 'expo-router';
 import { useEffect } from 'react';
 import { Touchable, TouchableOpacity } from 'react-native';
+import * as SecureStore from 'expo-secure-store';
+import { ClerkProvider, useAuth } from '@clerk/clerk-expo';
 
+const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
 
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (error) {
+      return null;
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (error) {
+      return;
+    }
+  },
+};
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -42,33 +61,58 @@ export default function RootLayout() {
     return null;
   }
 
-  return <RootLayoutNav />;
+  // wrap the RootLayoutNav by ClerkProvider
+  // return <RootLayoutNav />;
+  return (
+    <ClerkProvider
+      publishableKey={CLERK_PUBLISHABLE_KEY!}
+      tokenCache={tokenCache}
+    >
+      <RootLayoutNav />
+    </ClerkProvider>
+  );
 }
 
 function RootLayoutNav() {
   const router = useRouter();
+  // obtain auth status from clerk
+  const { isLoaded, isSignedIn } = useAuth();
+
+  // Automatic trigger login page
+  useEffect(() => {
+    if (isLoaded && !isSignedIn) {
+      router.push('/(modals)/login');
+    }
+  }, [isLoaded]);
+
   return (
-      <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="(modals)/login" options={{
-        title: 'Log in or sign up',
-        presentation: 'modal',
-        headerLeft: () => (
-          <TouchableOpacity onPress={() => router.back()}>
-            <Ionicons name="close-outline" size={28}/>
-          </TouchableOpacity>
-        )
-      }} />
-      <Stack.Screen name="listing/[id]" options={{ headerTitle: '' }} />
-      <Stack.Screen name="(modals)/booking" options={{
-        presentation: 'transparentModal',
-        animation: 'fade',
-        headerLeft: () => (
-          <TouchableOpacity onPress={() => router.back()}>
-            <Ionicons name="close-outline" size={28}/>
-          </TouchableOpacity>
-        )
-      }} />
-      </Stack>
+    <Stack>
+      <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+      <Stack.Screen
+        name='(modals)/login'
+        options={{
+          title: 'Log in or sign up',
+          presentation: 'modal',
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => router.back()}>
+              <Ionicons name='close-outline' size={28} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <Stack.Screen name='listing/[id]' options={{ headerTitle: '' }} />
+      <Stack.Screen
+        name='(modals)/booking'
+        options={{
+          presentation: 'transparentModal',
+          animation: 'fade',
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => router.back()}>
+              <Ionicons name='close-outline' size={28} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+    </Stack>
   );
 }
--- app/listing/[id].tsx
+++ app/listing/[id].tsx
@@ -1,12 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { useLocalSearchParams } from 'expo-router'
+import { View, Text } from 'react-native';
+import React from 'react';
+import { useLocalSearchParams } from 'expo-router';
 
-
+// Since listing folder is not within (tabs) pages here will not be displayed inside tabs
 const Page = () => {
   const { id } = useLocalSearchParams<{ id: string }>();
-  console.log("🚀 ~ file: [id].tsx:8 ~ Page ~ id:", id)
-    
+  console.log('🚀 ~ file: [id].tsx:8 ~ Page ~ id:', id);
+
   return (
     <View>
       <Text>Page</Text>
@@ -14,4 +14,4 @@ const Page = () => {
   );
 };
 
-export default Page
+export default Page;
