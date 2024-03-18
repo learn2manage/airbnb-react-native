1e97a3b21562d74ac04e8e8cf46fddb58307b09f
Learn2Manage <Learn2Manage@gmail.com>
2024-03-18 16:53:32 +0800

    Prepare layout of login page

--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -1,7 +1,15 @@
-import { View, Text, StyleSheet, TextInput } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 import React from 'react';
 import { useWarmUpBrowser } from '@/hooks/useWarmUpBrowser';
 import { defaultStyles } from '@/constants/Styles';
+import Colors from '@/constants/Colors';
+import { Ionicons } from '@expo/vector-icons';
 
 const Page = () => {
   useWarmUpBrowser();
@@ -11,8 +19,66 @@ const Page = () => {
       <TextInput
         autoCapitalize='none'
         placeholder='Email'
-        style={[defaultStyles.inputField]}
+        style={[defaultStyles.inputField, { marginBottom: 30 }]}
       />
+      <TouchableOpacity style={defaultStyles.btn}>
+        <Text style={defaultStyles.btnText}>Continue</Text>
+      </TouchableOpacity>
+      <View style={styles.separatorView}>
+        <View
+          style={{
+            flex: 1,
+            borderBottomColor: '#000',
+            borderBottomWidth: StyleSheet.hairlineWidth,
+          }}
+        />
+        <Text style={styles.separator}>or</Text>
+        <View
+          style={{
+            flex: 1,
+            borderBottomColor: '#000',
+            borderBottomWidth: StyleSheet.hairlineWidth,
+          }}
+        />
+      </View>
+      {/* Sign up buttons with different options */}
+      <View style={{ gap: 20 }}>
+        {/* Phone */}
+        <TouchableOpacity style={styles.btnOutline}>
+          <Ionicons
+            name='call-outline'
+            size={24}
+            style={defaultStyles.btnIcon}
+          />
+          <Text style={styles.btnOutlineText}>Continue with Phone</Text>
+        </TouchableOpacity>
+
+        {/* Apple */}
+        <TouchableOpacity style={styles.btnOutline}>
+          <Ionicons name='logo-apple' size={24} style={defaultStyles.btnIcon} />
+          <Text style={styles.btnOutlineText}>Continue with Apple</Text>
+        </TouchableOpacity>
+
+        {/* Google */}
+        <TouchableOpacity style={styles.btnOutline}>
+          <Ionicons
+            name='logo-google'
+            size={24}
+            style={defaultStyles.btnIcon}
+          />
+          <Text style={styles.btnOutlineText}>Continue with Google</Text>
+        </TouchableOpacity>
+
+        {/* Facebook */}
+        <TouchableOpacity style={styles.btnOutline}>
+          <Ionicons
+            name='logo-facebook'
+            size={24}
+            style={defaultStyles.btnIcon}
+          />
+          <Text style={styles.btnOutlineText}>Continue with Facebook</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -22,6 +88,32 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     padding: 26,
+  },
+  separatorView: {
+    flexDirection: 'row',
+    gap: 10,
+    alignItems: 'center',
+    marginVertical: 30,
+  },
+  separator: {
+    fontFamily: 'mon-sb',
+    color: Colors.grey,
+  },
+  btnOutline: {
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: Colors.grey,
+    height: 50,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    paddingHorizontal: 10,
+  },
+  btnOutlineText: {
+    color: '#000',
+    fontSize: 16,
+    fontFamily: 'mon-sb',
   },
 });
 export default Page;
