9812d7f4d18aecbb8088e4c23337157029d77614
Learn2manage <learn2manage@gmail.com>
2024-03-22 21:25:14 +0800

    Render the listings with a clickable Link

--- components/Listings.tsx
+++ components/Listings.tsx
@@ -1,18 +1,31 @@
-import { View, Text } from 'react-native';
-import React, { useEffect } from 'react';
+import { View, Text, FlatList, ListRenderItem } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { Link } from 'expo-router';
 
 interface Props {
   listings: any[];
   category: string;
 }
 
-const Listings = ({ listings, category }: Props) => {
+const Listings = ({ listings: items , category }: Props) => {
+  const [loading, setLoading] = useState(false);
+  const listRef = useRef<FlatList>(null);
+
   useEffect(() => {
-    console.log('RELOAD LISTINGS: ', listings.length);
+    console.log('RELOAD LISTINGS: ', items.length);
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 200)
   }, [category]);
+
+  const renderRow: ListRenderItem<any> = ({ item }) => (
+  <Link href={`/listing/${item.id}`}>Go there</Link>
+)
+
   return (
     <View style={{ flex: 1, marginTop: 5, backgroundColor: '#8ee490' }}>
-      <Text>Listings {category}</Text>
+      <FlatList renderItem={renderRow} ref={listRef} data={ loading? [] : items } />
     </View>
   );
 };
