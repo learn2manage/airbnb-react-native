04382b4179e72bfe6c43aa30653779974da5e783
Learn2Manage <Learn2Manage@gmail.com>
2024-03-27 17:31:12 +0800

    Handle details page header display. Buttons currently not visible

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,105 +10,104 @@ import { ClerkProvider, useAuth } from '@clerk/clerk-expo';
 const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
 
 const tokenCache = {
-    async getToken(key: string) {
-        try {
-            return SecureStore.getItemAsync(key);
-        } catch (error) {
-            return null;
-        }
-    },
-    async saveToken(key: string, value: string) {
-        try {
-            return SecureStore.setItemAsync(key, value);
-        } catch (error) {
-            return;
-        }
-    },
+  async getToken(key: string) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (error) {
+      return null;
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (error) {
+      return;
+    }
+  },
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-    const [loaded, error] = useFonts({
-        mon: require('../assets/fonts/Montserrat-Regular.ttf'),
-        'mon-sb': require('../assets/fonts/Montserrat-SemiBold.ttf'),
-        'mon-b': require('../assets/fonts/Montserrat-Bold.ttf'),
-    });
+  const [loaded, error] = useFonts({
+    mon: require('../assets/fonts/Montserrat-Regular.ttf'),
+    'mon-sb': require('../assets/fonts/Montserrat-SemiBold.ttf'),
+    'mon-b': require('../assets/fonts/Montserrat-Bold.ttf'),
+  });
 
-    // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-    useEffect(() => {
-        if (error) throw error;
-    }, [error]);
+  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
 
-    useEffect(() => {
-        if (loaded) {
-            SplashScreen.hideAsync();
-        }
-    }, [loaded]);
-
-    if (!loaded) {
-        return null;
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
     }
+  }, [loaded]);
 
-    // wrap the RootLayoutNav by ClerkProvider
-    // return <RootLayoutNav />;
-    return (
-        <ClerkProvider
-            publishableKey={CLERK_PUBLISHABLE_KEY!}
-            tokenCache={tokenCache}
-        >
-            <RootLayoutNav />
-        </ClerkProvider>
-    );
+  if (!loaded) {
+    return null;
+  }
+
+  // wrap the RootLayoutNav by ClerkProvider
+  // return <RootLayoutNav />;
+  return (
+    <ClerkProvider
+      publishableKey={CLERK_PUBLISHABLE_KEY!}
+      tokenCache={tokenCache}>
+      <RootLayoutNav />
+    </ClerkProvider>
+  );
 }
 
 function RootLayoutNav() {
-    // obtain auth status from clerk
-    const { isLoaded, isSignedIn } = useAuth();
-    const router = useRouter();
+  // obtain auth status from clerk
+  const { isLoaded, isSignedIn } = useAuth();
+  const router = useRouter();
 
-    // Automatic trigger login page
-    useEffect(() => {
-        if (isLoaded && !isSignedIn) {
-            router.push('/(modals)/login');
-        }
-    }, [isLoaded]);
+  // Automatic trigger login page
+  useEffect(() => {
+    if (isLoaded && !isSignedIn) {
+      router.push('/(modals)/login');
+    }
+  }, [isLoaded]);
 
-    return (
-        <Stack>
-            <Stack.Screen
-                name="(modals)/login"
-                options={{
-                    title: 'Log in or sign up',
-                    presentation: 'modal',
-                    headerTitleStyle: {
-                        fontFamily: 'mon-sb',
-                    },
-                    headerLeft: () => (
-                        <TouchableOpacity onPress={() => router.back()}>
-                            <Ionicons name="close-outline" size={28} />
-                        </TouchableOpacity>
-                    ),
-                }}
-            />
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen
-                name="listing/[id]"
-                options={{ headerTitle: '', headerTransparent: false }}
-            />
-            <Stack.Screen
-                name="(modals)/booking"
-                options={{
-                    presentation: 'transparentModal',
-                    animation: 'fade',
-                    headerLeft: () => (
-                        <TouchableOpacity onPress={() => router.back()}>
-                            <Ionicons name="close-outline" size={28} />
-                        </TouchableOpacity>
-                    ),
-                }}
-            />
-        </Stack>
-    );
+  return (
+    <Stack>
+      <Stack.Screen
+        name='(modals)/login'
+        options={{
+          title: 'Log in or sign up',
+          presentation: 'modal',
+          headerTitleStyle: {
+            fontFamily: 'mon-sb',
+          },
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => router.back()}>
+              <Ionicons name='close-outline' size={28} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+      <Stack.Screen
+        name='listing/[id]'
+        options={{ headerTitle: '', headerTransparent: true }}
+      />
+      <Stack.Screen
+        name='(modals)/booking'
+        options={{
+          presentation: 'transparentModal',
+          animation: 'fade',
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => router.back()}>
+              <Ionicons name='close-outline' size={28} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+    </Stack>
+  );
 }
