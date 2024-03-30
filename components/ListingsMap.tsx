9ca321840c6680fb28f6f1174cdf916f21a14bb0
Learn2manage <learn2manage@gmail.com>
2024-03-30 09:39:09 +0800

    Finally able to focus the map on current category region.

--- components/ListingsMap.tsx
+++ components/ListingsMap.tsx
@@ -1,8 +1,10 @@
-import { View, Text, StyleSheet } from 'react-native';
-import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
 import MapView, { Marker } from 'react-native-maps';
 import { ListingGeo } from '@/interfaces/listingGeo';
 import { useRouter } from 'expo-router';
+import Colors from '@/constants/Colors';
+import { Ionicons } from '@expo/vector-icons';
 
 interface Props {
     listings: any;
@@ -17,17 +19,64 @@ const INITIAL_REGION = {
 
 const ListingsMap = ({ listings }: Props) => {
     const router = useRouter();
+    const mapRef = useRef<any>(null);
+    const [region, setRegion] = useState<any>(null);
+
     const onMarkerSelected = (item: ListingGeo) => {
         router.push(`/listing/${item.properties.id}`);
+    };
+
+    useEffect(() => {
+        onLocateMe();
+    }, []);
+
+    useEffect(() => {
+        if (listings.length > 0) {
+            const newRegion = {
+                latitude: parseFloat(listings[0].properties.latitude),
+                longitude: parseFloat(listings[0].properties.longitude),
+                latitudeDelta: 0.01,
+                longitudeDelta: 0.01,
+            };
+            setRegion(newRegion);
+        }
+    }, [listings]);
+
+    useEffect(() => {
+        console.log(
+            '🚀 ~ file: ListingsMap.tsx:42 ~ useEffect ~ region:',
+            region
+        );
+        if (region) {
+            mapRef.current.animateToRegion(region, 1000);
+        }
+    }, [region]);
+
+    const onLocateMe = () => {
+        const region = {
+            //latitude: 22.3343,
+            //longitude: 114.9327,
+            latitude: parseFloat(listings[0].properties.latitude),
+            longitude: parseFloat(listings[0].properties.longitude),
+            latitudeDelta: 0.01,
+            longitudeDelta: 0.01,
+        };
+        console.log(
+            '🚀 ~ file: ListingsMap.tsx:62 ~ onLocateMe ~ region:',
+            region
+        );
+
+        mapRef.current?.animateToRegion(region);
     };
 
     return (
         <View style={styles.container}>
             <MapView
+                ref={mapRef}
                 style={StyleSheet.absoluteFill}
                 showsUserLocation
                 showsMyLocationButton
-                initialRegion={INITIAL_REGION}
+                initialRegion={region}
             >
                 {listings.map((item: ListingGeo) => (
                     <Marker
@@ -46,6 +95,9 @@ const ListingsMap = ({ listings }: Props) => {
                     </Marker>
                 ))}
             </MapView>
+            <TouchableOpacity style={styles.locateBtn} onPress={onLocateMe}>
+                <Ionicons name="navigate" size={24} color={Colors.dark} />
+            </TouchableOpacity>
         </View>
     );
 };
@@ -74,6 +126,22 @@ const styles = StyleSheet.create({
     markerText: {
         fontSize: 14,
         fontFamily: 'mon-sb',
+    },
+    locateBtn: {
+        position: 'absolute',
+        top: 70,
+        right: 20,
+        backgroundColor: '#fff',
+        padding: 10,
+        borderRadius: 10,
+        elevation: 2,
+        shadowColor: '#000',
+        shadowOpacity: 0.1,
+        shadowRadius: 6,
+        shadowOffset: {
+            width: 1,
+            height: 10,
+        },
     },
 });
 
