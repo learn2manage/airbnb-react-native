eecc5c79a34d1d99810218955490d64fb8b02ae5
Learn2manage <learn2manage@gmail.com>
2024-04-02 20:39:42 +0800

    Handle Profile page components for image upload and edit user name.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,64 +1,81 @@
 import { Tabs } from 'expo-router';
 import Colors from '@/constants/Colors';
-import { FontAwesome5, Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
+import {
+    FontAwesome5,
+    Ionicons,
+    MaterialCommunityIcons,
+} from '@expo/vector-icons';
 
 const Layout = () => {
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors.primary,
-        tabBarLabelStyle: {
-          fontFamily: 'mon-sb',
-        },
-      }}
-    >
-      <Tabs.Screen
-        name='index'
-        options={{
-          tabBarLabel: 'Explore',
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name='search' size={size} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name='wishlists'
-        options={{
-          tabBarLabel: 'Wishlists',
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name='heart-outline' size={size} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name='trips'
-        options={{
-          tabBarLabel: 'Trips',
-          tabBarIcon: ({ color, size }) => (
-            <FontAwesome5 name='airbnb' size={size} color={color} />
-          ),
-        }}
-      />
+    return (
+        <Tabs
+            screenOptions={{
+                tabBarActiveTintColor: Colors.primary,
+                tabBarLabelStyle: {
+                    fontFamily: 'mon-sb',
+                },
+            }}
+        >
             <Tabs.Screen
-        name='inbox'
-        options={{
-          tabBarLabel: 'Inbox',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name='message-outline' size={size} color={color} />
-          ),
-        }}
-      />
+                name="index"
+                options={{
+                    tabBarLabel: 'Explore',
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="search" size={size} color={color} />
+                    ),
+                }}
+            />
             <Tabs.Screen
-        name='profile'
-        options={{
-          tabBarLabel: 'Profile',
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name='person-circle-outline' size={size} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
-  );
+                name="wishlists"
+                options={{
+                    tabBarLabel: 'Wishlists',
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons
+                            name="heart-outline"
+                            size={size}
+                            color={color}
+                        />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="trips"
+                options={{
+                    tabBarLabel: 'Trips',
+                    tabBarIcon: ({ color, size }) => (
+                        <FontAwesome5 name="airbnb" size={size} color={color} />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="inbox"
+                options={{
+                    tabBarLabel: 'Inbox',
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons
+                            name="message-outline"
+                            size={size}
+                            color={color}
+                        />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="profile"
+                options={{
+                    tabBarLabel: 'Profile',
+                    headerShown: false,
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons
+                            name="person-circle-outline"
+                            size={size}
+                            color={color}
+                        />
+                    ),
+                }}
+            />
+        </Tabs>
+    );
 };
 
 export default Layout;
