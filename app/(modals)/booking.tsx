70813b515887c038ae57f076684b9adbb9346ac1
Learn2manage <learn2manage@gmail.com>
2024-04-02 21:57:51 +0800

    Prepareing initial codes for booking.tsx and its ModalHeaderText. Note that this feature is not looking good on Android.

--- app/(modals)/booking.tsx
+++ app/(modals)/booking.tsx
@@ -1,12 +1,133 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React from 'react';
+import Colors from '@/constants/Colors';
+import Animated from 'react-native-reanimated';
+import { BlurView } from 'expo-blur';
 
-const Page = () => {
-  return (
-    <View>
-      <Text>Booking</Text>
-    </View>
-  )
-}
+const AnimatedTouchableOpacity =
+    Animated.createAnimatedComponent(TouchableOpacity);
 
-export default Page
+const guestsGropus = [
+    {
+        name: 'Adults',
+        text: 'Ages 13 or above',
+        count: 0,
+    },
+    {
+        name: 'Children',
+        text: 'Ages 2-12',
+        count: 0,
+    },
+    {
+        name: 'Infants',
+        text: 'Under 2',
+        count: 0,
+    },
+    {
+        name: 'Pets',
+        text: 'Pets allowed',
+        count: 0,
+    },
+];
+
+const booking = () => {
+    return (
+        <BlurView intensity={70} style={styles.container} tint="light">
+            <Text>booking</Text>
+        </BlurView>
+    );
+};
+
+export default booking;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 100,
+    },
+    card: {
+        backgroundColor: '#fff',
+        borderRadius: 14,
+        margin: 10,
+        elevation: 4,
+        shadowColor: '#000',
+        shadowOpacity: 0.3,
+        shadowRadius: 4,
+        shadowOffset: {
+            width: 2,
+            height: 2,
+        },
+        gap: 20,
+    },
+    cardHeader: {
+        fontFamily: 'mon-b',
+        fontSize: 24,
+        padding: 20,
+    },
+    cardBody: {
+        paddingHorizontal: 20,
+        paddingBottom: 20,
+    },
+    cardPreview: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 20,
+    },
+
+    searchSection: {
+        height: 50,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        borderWidth: 1,
+        borderColor: '#ABABAB',
+        borderRadius: 8,
+        marginBottom: 16,
+    },
+    searchIcon: {
+        padding: 10,
+    },
+    inputField: {
+        flex: 1,
+        padding: 10,
+        backgroundColor: '#fff',
+    },
+    placesContainer: {
+        flexDirection: 'row',
+        gap: 25,
+    },
+    place: {
+        width: 100,
+        height: 100,
+        borderRadius: 10,
+    },
+    placeSelected: {
+        borderColor: Colors.grey,
+        borderWidth: 2,
+        borderRadius: 10,
+        width: 100,
+        height: 100,
+    },
+    previewText: {
+        fontFamily: 'mon-sb',
+        fontSize: 14,
+        color: Colors.grey,
+    },
+    previewdData: {
+        fontFamily: 'mon-sb',
+        fontSize: 14,
+        color: Colors.dark,
+    },
+
+    guestItem: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingVertical: 16,
+    },
+    itemBorder: {
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        borderBottomColor: Colors.grey,
+    },
+});
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,6 +6,8 @@ import { useEffect } from 'react';
 import { Touchable, TouchableOpacity } from 'react-native';
 import * as SecureStore from 'expo-secure-store';
 import { ClerkProvider, useAuth } from '@clerk/clerk-expo';
+import ModalHeaderText from '@/components/ModalHeaderText';
+import Colors from '@/constants/Colors';
 
 const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
 
@@ -98,9 +100,20 @@ function RootLayoutNav() {
                 name="(modals)/booking"
                 options={{
                     presentation: 'transparentModal',
+                    headerTransparent: true,
+                    headerTitle: () => <ModalHeaderText />,
                     animation: 'fade',
                     headerLeft: () => (
-                        <TouchableOpacity onPress={() => router.back()}>
+                        <TouchableOpacity
+                            onPress={() => router.back()}
+                            style={{
+                                backgroundColor: '#817e7e',
+                                borderColor: Colors.grey,
+                                borderRadius: 20,
+                                borderWidth: 1,
+                                padding: 4,
+                            }}
+                        >
                             <Ionicons name="close-outline" size={28} />
                         </TouchableOpacity>
                     ),
